b69ec69b5726018ebdc2d736004bd2add01c9071
Oleg <oleg.pav.m@gmail.com>
2018-03-06 21:04:51 +0300

    Logger

--- Util/Logger.rb
+++ Util/Logger.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 module Util
 	class Logger
 
@@ -5,7 +7,63 @@ module Util
 			@time = time
 		end
 
-		#
+		def start
+			log 'Start', :bold
+		end
+
+		def buy
+			log 'Buy'
+		end
+
+		def buy_profit
+			log 'Close buy', :blue
+		end
+
+		def buy_fail
+			log 'Close buy', :yellow
+		end
+
+		def sell
+			log 'Sell'
+		end
+
+		def sell_profit
+			log 'Close sell', :blue
+		end
+
+		def sell_fail
+			log 'Close sell', :yellow
+		end
+
+		def error(prefix, value)
+			log "ERROR #{prefix} -> #{value}", :red
+		end
+
+		def empty
+			log
+		end
+
+		def result(prefix, value)
+			log "RESULT #{prefix} -> #{value.round(2)}", :bold
+		end
+
+		private
+
+		def log(value = nil, color = nil)
+			if value
+				value = "#{@time.now_text} - #{value}"
+			else
+				value = @time.now_text
+			end
+
+			if color
+				puts value.send(color)
+			else
+				puts value
+			end
+			
+			File.write('Storage/log.txt', "#{value}\n", mode: 'a')
+		end
 
 	end
 end
