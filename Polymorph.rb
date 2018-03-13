73c564dcb92c1552cbc7344408786cb448be339d
Oleg <oleg.pav.m@gmail.com>
2018-03-13 17:55:17 +0300

    Tick

--- Polymorph.rb
+++ Polymorph.rb
@@ -33,21 +33,39 @@ class Trigger
 
 end
 
+# TODO Check ma calc
 class Tick
-	attr_reader :index,
+	attr_reader :date,
 		:open, :high, :low, :close,
 		:red, :green,
 		:ma_cross, :prev_ma_cross
 
-	def initialize(connector)
+	def initialize(connector, red_period, green_period)
 		@connector = connector
+		@history = connector.history.dup
+		@red_period = red_period
+		@green_period = green_period
+
+		update_by_history
 	end
 
-	def update
-		_, @open, @high, @low, @close = @connector.raw_tick
-		@index = @connector.raw_tick_index
+	def update(data = nil)
+		unless data
+			data = @connector.raw_tick
+			@history << data
+		end
 
-		# TODO MA
+		@date, @open, @high, @low, @close = @history.last
+
+		last = @history.length
+
+		red = @history.sma(last, @red_period)
+		green = @history.sma(last, @green_period)
+
+		if (red >= green and @red <= @green) or (red < green and @red > @green)
+			@prev_ma_cross = @ma_cross
+			@ma_cross = @date
+		end
 	end
 
 	def break?
@@ -55,6 +73,34 @@ class Tick
 		(high > red   and low < red  )
 	end
 
+	private
+
+	def update_by_history
+		last_date = nil
+		last_red = nil
+		last_green = nil
+
+		@history[0...-1].each.with_index do |tick, index|
+			next if index < @red_period
+
+			if index == @red_period
+				last_date = tick[0]
+				last_red = @history.sma(index, @red_period)
+				last_green = @history.sma(index, @green_period)
+				next
+			end
+
+			red = @history.sma(index, @red_period)
+			green = @history.sma(index, @green_period)
+
+			if (red >= green and last_red <= last_green) or (red < green and last_red > last_green)
+				@prev_ma_cross = @ma_cross
+				@ma_cross = last_date
+			end
+		end
+
+		update(@history.last)
+	end
 
 end
 
@@ -86,10 +132,6 @@ class Connector
 	end
 
 	def raw_tick
-		#
-	end
-
-	def raw_tick_index
 		#
 	end
 
@@ -138,12 +180,16 @@ class PolymorphLang
 	end
 
 	def double_ma_cross?
-		@connector.position_tick.index < @tick.prev_ma_cross
+		if @tick.prev_ma_cross
+			@connector.position_tick.date < @tick.prev_ma_cross
+		else
+			false
+		end
 	end
 
 	def so_fast_return?
-		@trigger.prepare_tick.index > @trigger.tick.index and
-		@trigger.prepare_tick.index - @trigger.tick.index <= (@opt.fast_trig_return + 1)
+		@trigger.prepare_tick.date > @trigger.tick.date and
+		@trigger.prepare_tick.date - @trigger.tick.date <= (@opt.fast_trig_return + 1) * @opt.resolution
 	end
 
 	def current_is_break?
@@ -179,11 +225,11 @@ class PolymorphLang
 	end
 
 	def trig?
-		@trigger.tick.index == @tick.index
+		@trigger.tick.date == @tick.date
 	end
 
 	def so_fast_trig?
-		@trigger.tick.index - @trigger.prepare_tick.index <= (@opt.fast_trig + 1)
+		@trigger.tick.date - @trigger.prepare_tick.date <= (@opt.fast_trig + 1) * @opt.resolution
 	end
 
 	def squeeze?
