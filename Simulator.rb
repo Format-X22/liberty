9e12a00400bce262ad7fefa4028741b6cbf6a78f
Oleg <oleg.pav.m@gmail.com>
2018-03-19 04:44:01 +0300

    Simulator #3

--- Simulator.rb
+++ Simulator.rb
@@ -10,6 +10,8 @@ class Simulator < ConnectorInterface
 		@history = []
 		@position = Position.new
 
+		@deposit = 1.0
+
 		# TODO logger
 	end
 
@@ -44,20 +46,9 @@ class Simulator < ConnectorInterface
 	def cycle(&iteration)
 		@data.each do |raw|
 			@candle = Candle.new(raw)
-
 			@history << @candle
 
-			if position?
-				if @position.type == :long
-					if @candle.high > @position.exit_price
-						# TODO buy logic
-					end
-				else
-					if @candle.low < @position.exit_price
-						# TODO sell logic
-					end
-				end
-			end
+			handle_position if position?
 
 			iteration.call
 		end
@@ -65,6 +56,39 @@ class Simulator < ConnectorInterface
 
 	private
 
-	#
+	def handle_position
+		if @position.type == :long
+			if @candle.low <= @position.fail_price
+				fail(@position.fail_price, 1)
+			elsif @candle.high > @position.exit_price
+				profit(@position.exit_price, 1)
+			end
+		else
+			if @candle.high >= @position.fail_price
+				fail(@position.fail_price, -1)
+			elsif @candle.low < @position.exit_price
+				profit(@position.exit_price, -1)
+			end
+		end
+	end
+
+	def profit(price, direction_mul)
+		# TODO log profit
+
+		close_position(price, direction_mul)
+	end
+
+	def fail(price, direction_mul)
+		# TODO log fail
+
+		close_position(price, direction_mul)
+	end
+
+	def close_position(close_price, direction_mul)
+		amount = direction_mul * @deposit * @opt.mul
+		basic_profit = (1 / @position.candle.close - 1 / close_price)
+
+		@deposit += amount * basic_profit
+	end
 
 end
